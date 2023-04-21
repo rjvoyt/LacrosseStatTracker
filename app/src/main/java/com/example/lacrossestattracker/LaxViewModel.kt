dd99fafec10a080e1e9cb933c493aaadd3861720
rjvoyt <rjvoytek@icloud.com>
2023-04-21 13:01:30 -0400

    day3

--- app/src/main/java/com/example/lacrossestattracker/LaxViewModel.kt
+++ app/src/main/java/com/example/lacrossestattracker/LaxViewModel.kt
@@ -2,7 +2,37 @@ package com.example.lacrossestattracker
 
 import androidx.lifecycle.ViewModel
 
-class LaxViewModel: ViewModel() {
-    lateinit var teams :MutableList<Team>
 
+class LaxViewModel : ViewModel() {
+    private lateinit var teams: MutableList<Team>
+    private lateinit var _currentTeam: Team
+    val currentTeam: Team
+        get() = _currentTeam
+    private lateinit var _currentTeamName: String
+    val currentTeamName: String
+        get() = _currentTeamName
+    private lateinit var _currentPlayerList: MutableList<Player>
+    val currentPlayerList: MutableList<Player>
+        get() = _currentPlayerList
+    private lateinit var _currentPlayer: Player
+    val currentPlayer: Player
+        get() = _currentPlayer
+    private var _currentPlayerFirstName= ""
+    val currentPlayerFirstName: String
+        get() = _currentPlayerFirstName
+    private var _currentPlayerLastName= ""
+    val currentPlayerLastName: String
+        get() = _currentPlayerLastName
+    private var _currentPlayerNumber = 0
+    val currentPlayerNumber: Int
+        get() = _currentPlayerNumber
+    private var _currentPlayerGoals = 0
+    val currentPlayerGoals: Int
+        get() = _currentPlayerGoals
+    private var _currentPlayerGroundBalls = 0
+    val currentPlayerGroundBalls: Int
+        get() = _currentPlayerGroundBalls
+    private var _currentPlayerAssists = 0
+    val currentPlayerAssists: Int
+        get() = _currentPlayerAssists
 }
--- app/src/main/java/com/example/lacrossestattracker/WelcomeFragment.kt
+++ app/src/main/java/com/example/lacrossestattracker/WelcomeFragment.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 class WelcomeFragment : Fragment() {
-
+    lateinit var dbRef : DatabaseReference
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        dbRef = Firebase.database.reference
         return inflater.inflate(R.layout.fragment_welcome, container, false)
     }
 
