a2d3dfa7b1037d5dcd793c2bd135066f61934e30
rjvoyt <rjvoytek@icloud.com>
2023-05-02 12:54:45 -0400

    commit

--- app/src/main/java/com/example/lacrossestattracker/TeamFragment.kt
+++ app/src/main/java/com/example/lacrossestattracker/TeamFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.lacrossestattracker.databinding.FragmentTeamBinding
 
 
 class TeamFragment : Fragment() {
-
+    private var _binding: FragmentTeamBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_team, container, false)
+        _binding = FragmentTeamBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+        return rootView
     }
 
 }
--- app/src/main/java/com/example/lacrossestattracker/WelcomeFragment.kt
+++ app/src/main/java/com/example/lacrossestattracker/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.lacrossestattracker.databinding.FragmentWelcomeBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -12,12 +14,20 @@ import com.google.firebase.ktx.Firebase
 
 class WelcomeFragment : Fragment() {
     lateinit var dbRef : DatabaseReference
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        val rootView = binding.root
         dbRef = Firebase.database.reference
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        binding.getStartedButton.setOnClickListener{
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToTeamFragment()
+            binding.root.findNavController().navigate(action)
+        }
+        return rootView
     }
 
 }
