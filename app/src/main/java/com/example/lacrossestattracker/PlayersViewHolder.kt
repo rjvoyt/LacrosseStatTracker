7c987aa77f7768746fcfc2deb9088477716e7fc6
rjvoyt <rjvoytek@icloud.com>
2023-05-03 12:59:57 -0400

    commit

---
+++ app/src/main/java/com/example/lacrossestattracker/PlayersViewHolder.kt
@@ -0,0 +1,21 @@
+package com.example.lacrossestattracker
+
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lacrossestattracker.databinding.ListPlayerLayoutBinding
+
+
+class PlayersViewHolder(val binding: ListPlayerLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    private lateinit var currentPlayer: Player
+    init {
+        binding.root.setOnClickListener {
+            val action = PlayersFragmentDirections.actionPlayersFragmentToStatsFragment()
+            binding.root.findNavController().navigate(action)
+        }
+    }
+    fun bindTeam(player: Player) {
+        currentPlayer = player
+        binding.playerNameTextView.text = "${currentPlayer.firstName} ${currentPlayer.lastName}"
+        binding.playerNumberTextView.text = "#${currentPlayer.number}"
+    }
+}
---
+++ app/src/main/java/com/example/lacrossestattracker/TeamsViewHolder.kt
@@ -0,0 +1,19 @@
+package com.example.lacrossestattracker
+
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lacrossestattracker.databinding.ListTeamLayoutBinding
+
+class TeamsViewHolder(val binding: ListTeamLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    private lateinit var currentTeam: Team
+    init {
+        binding.root.setOnClickListener {
+            val action = TeamFragmentDirections.actionTeamFragmentToPlayersFragment()
+            binding.root.findNavController().navigate(action)
+        }
+    }
+    fun bindTeam(team: Team) {
+        currentTeam = team
+        binding.teamNameTextView.text = currentTeam.teamName
+    }
+}
