39905428cd8722ca70f888c8c1bd75c804366c4f
rjvoyt <rjvoytek@icloud.com>
2023-05-09 12:48:41 -0400

    commit

--- app/src/main/java/com/example/lacrossestattracker/StatsFragment.kt
+++ app/src/main/java/com/example/lacrossestattracker/StatsFragment.kt
@@ -19,7 +19,42 @@ class StatsFragment : Fragment() {
         _binding = FragmentStatsBinding.inflate(inflater, container, false)
         val rootView = binding.root
         val viewModel: LaxViewModel by viewModels()
+        binding.assistsNum.text = viewModel.currentPlayerAssists.toString()
+        binding.goalsNum.text = viewModel.currentPlayerGoals.toString()
+        binding.groundBallsNum.text = viewModel.currentPlayerGroundBalls.toString()
+        binding.savesNum.text = viewModel.currentPlayerSaves.toString()
+        binding.plusButtonGoals.setOnClickListener {
+            viewModel.editGoal(1)
+            binding.goalsNum.text = viewModel.currentPlayerGoals.toString()
+        }
+        binding.minusButtonGoals.setOnClickListener {
+            viewModel.editGoal(-1)
+            binding.goalsNum.text = viewModel.currentPlayerGoals.toString()
+        }
+        binding.plusButtonAssists.setOnClickListener {
+            viewModel.editAssist(1)
+            binding.assistsNum.text = viewModel.currentPlayerAssists.toString()
+        }
+        binding.minusButtonAssists.setOnClickListener {
+            viewModel.editAssist(-1)
+            binding.assistsNum.text = viewModel.currentPlayerAssists.toString()
+        }
+        binding.plusButtonGroundBalls.setOnClickListener {
+            viewModel.editGroundBall(1)
+            binding.groundBallsNum.text = viewModel.currentPlayerGroundBalls.toString()
+        }
+        binding.minusButtonGroundBalls.setOnClickListener {
+            viewModel.editGroundBall(-1)
+            binding.groundBallsNum.text = viewModel.currentPlayerGroundBalls.toString()
+        }
+        binding.plusButtonSaves.setOnClickListener {
+            viewModel.editSave(1)
+            binding.savesNum.text = viewModel.currentPlayerSaves.toString()
+        }
+        binding.minusButtonSaves.setOnClickListener {
+            viewModel.editSave(-1)
+            binding.savesNum.text = viewModel.currentPlayerSaves.toString()
+        }
         return rootView
     }
-
 }
