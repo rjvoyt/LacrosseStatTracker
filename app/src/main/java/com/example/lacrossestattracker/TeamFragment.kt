350c2f961ec49845f01f77985793cd333bb80530
rjvoyt <rjvoytek@icloud.com>
2023-05-11 13:02:10 -0400

    commit

--- app/src/main/java/com/example/lacrossestattracker/TeamFragment.kt
+++ app/src/main/java/com/example/lacrossestattracker/TeamFragment.kt
@@ -1,14 +1,16 @@
 package com.example.lacrossestattracker
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.lacrossestattracker.databinding.FragmentTeamBinding
 import androidx.fragment.app.viewModels
+import com.example.lacrossestattracker.databinding.TeamDialogLayoutBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-
 
 class TeamFragment : Fragment() {
     private var _binding: FragmentTeamBinding? = null
@@ -21,8 +23,20 @@ class TeamFragment : Fragment() {
         val rootView = binding.root
         val viewModel: LaxViewModel by viewModels()
         binding.addTeamButton.setOnClickListener {
-            val mAdapter = TeamsAdapter(viewModel.teams)
-            binding.teamsRecyclerView.adapter = mAdapter
+            val dialogView =
+                LayoutInflater.from(requireContext()).inflate(R.layout.team_dialog_layout, null)
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.add_team)
+                .setView(dialogView)
+                .setNegativeButton(R.string.cancel) { dialog, which ->
+                    dialog.dismiss()
+                }
+                .setPositiveButton(R.string.add){dialog, which->
+                    dialog.dismiss()
+                    if(dialogView.)
+                    val mAdapter = TeamsAdapter(viewModel.teams)
+                    binding.teamsRecyclerView.adapter = mAdapter
+                }
         }
         return rootView
     }
